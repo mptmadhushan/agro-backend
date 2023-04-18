80b36adae29d1c97c9bacc9c9a286b43aa9d17c3
mptmadhushan-insureme <Tharindu@digitalservices.lk>
2023-04-18 15:18:53 +0530

    first commit

---
+++ app/controllers/posts.controller.js
@@ -0,0 +1,65 @@
+const db = require("../models");
+const Posts = db.posts;
+const Comments = db.comments;
+const upload = require("../middleware/uploadArray");
+
+
+exports.createPost = async (req, res) => {
+  console.log(
+    "🚀 ~ fil: posts.controller.js ~ line 7 ~ exports.createPost= ~ req",
+    req.files
+  );
+  await upload(req, res);
+
+  if (req.files == undefined) {
+    return res.status(400).send({ message: "Please upload a file!" });
+  }
+  const photo = {
+    name:req.body.name,
+    lat:req.body.lat,
+    lon:req.body.lon,
+  
+  };
+
+  // Save Tutorial in the database
+  Posts.create(photo)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial.",
+      });
+    });
+};
+exports.findPostById = (req, res) => {
+  const postId = req.params.postId;
+
+  return Posts.findByPk(postId)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial.",
+      });
+    });
+};
+
+exports.findAll = (req, res) => {
+  return Posts.findAll({
+    // attributes: [
+    //   "*",
+    //   [db.Sequelize.fn('date_format', db.Sequelize.col('post.createdAt'), '%d %b %y'), 'col_name']
+
+    // ],
+    include: ["comments"],
+  }).then((package) => {
+    console.log(">> All tutorials", JSON.stringify(package, null, 2));
+    res.send(package);
+    // console.log(package);
+    // return package;
+  });
+};
